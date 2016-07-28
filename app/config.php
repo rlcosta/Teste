bf1b510ace7da5b2ca24c20e5af956cc9b54fb55
Romulo <romulo4961@gmail.com>
2016-07-28 12:51:05 -0300

    Rodar local htaccess alterado 3

--- app/config.php
+++ app/config.php
@@ -6,31 +6,32 @@
 
 	$configs->env->development->baseURI = '/teste/';
 
-	$configs->env->development->database->setConnectionData([
+	$configs->env->development->database->setConnectionData(array(
 			'driver' => 'mysql',
 			'host' => 'localhost',
 			'user' => 'root',
 			'password' => '',
 			'dbname' => 'sistemahx',
 			'charset' => 'utf8'
-		]);
+		));
 
 	$configs->env->development->auth->setURLs('/teste/home/','/teste/login/');
 
-
+/*
 	$configs->env->add('production');
 
 	$configs->env->production->baseURI = '/';
 
-	$configs->env->production->database->setConnectionData([
+	$configs->env->production->database->setConnectionData(array()
 			'driver' => 'mysql',
 			'host' => 'localhost',
 			'user' => '	u942981625_arant',
 			'password' => 'tgPJ6jxDW40lMP',
 			'dbname' => 'u942981625_banco',
 			'charset' => 'utf8'
-		]);
+		));
 
 	$configs->env->production->auth->setURLs('/home/','/login/');
+*/
 
 	return $configs;
